59466d55a136b92003605e84d226fa7164939e82
EnigmaXyl <xiongyilun@gmail.com>
2024-05-14 10:32:02 +0800

    feat: Trading System Update

--- webull-java-sdk-core/src/main/java/com/webull/openapi/config/ProjectReaderHelper.java
+++ webull-java-sdk-core/src/main/java/com/webull/openapi/config/ProjectReaderHelper.java
@@ -24,7 +24,7 @@ public class ProjectReaderHelper {
 
     private static final String APPLICATION_VERSION = "application.version";
 
-    private static final String DEFAULT_APPLICATION_VERSION = "0.2.4";
+    private static final String DEFAULT_APPLICATION_VERSION = "0.2.5";
 
     private static final String APPLICATION_NAME = "application.name";
 
